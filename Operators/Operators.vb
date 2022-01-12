00865f90159722088ef43263c0cffcf92eb39542
Jamison Burton <jamison@burtontalk.com>
2022-01-12 08:44:24 -0700

    Add project files.

---
+++ Operators/Operators.vb
@@ -0,0 +1,32 @@
+﻿Module Operators
+
+    Sub Main()
+        'Example of + Operator
+        Console.Write("5 + 5 = ")
+        Console.WriteLine(5 + 5)
+        Console.WriteLine() 'Adds a space between the next line
+        'Example of * Operator
+        Console.Write("5 * 5 = ")
+        Console.WriteLine(5 * 5)
+        Console.WriteLine() 'Adds a space between the next line
+        'Example of ^ Operator
+        Console.Write("5 ^ 2 = ")
+        Console.WriteLine(5 ^ 2)
+        Console.WriteLine() 'Adds a space between the next line
+        'Example of / Operator
+        Console.Write("5 / 2 = ")
+        Console.WriteLine(5 / 2)
+        Console.WriteLine() 'Adds a space between the next line
+        'Example of \ Operator, Interger Division
+        Console.Write("5 \ 2 = ")
+        Console.WriteLine(5 \ 2)
+        Console.WriteLine() 'Adds a space between the next line
+        'Example of Mod Operator, modulus remainder
+        Console.Write("5 Mod 2 = ")
+        Console.WriteLine(5 Mod 2)
+
+        Console.Beep()
+        Console.Read() 'Wait for User response here
+    End Sub
+
+End Module
